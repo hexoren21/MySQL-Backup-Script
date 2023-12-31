91c2b406bebf4b5317091320cfde61cd95454d53
hexoren21 <kapel21@o2.pl>
2023-12-31 11:54:44 +0000

    local backup with email notify

--- script1.sh
+++ script1.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# nazwa oraz sciezka
+# name and path
 DB_NAME="exampledb"
 BACKUP_DIR="/home/przemyslawk/backup/"
 
-# utworzenie kopii 
+# create backup 
 DATE=$(date +%Y%m%d_%H%M%S)
 BACKUP_FILE="$BACKUP_DIR/$DB_NAME_$DATE.sql"
+
+# Address e-mail
+EMAIL="kapel21@o2.pl"
 
 mysqldump $DB_NAME > $BACKUP_FILE
 
 if [ $? -eq 0 ]; then
 	echo "Backup od $DB_NAME completed successfully"
 else
-	echo "Error in creating Backup od $DB_NAME"
+	# sending e-mail 
+	SUBJECT="Alert: problem with backup"
+	MESSAGE="Error in creating Backup from $DB_NAME on machine $HOSTNAME"
+
+	echo -e "Subject: $SUBJECT\n\n$MESSAGE" | msmtp $EMAIL
+	echo "wyslano maila"
 fi
